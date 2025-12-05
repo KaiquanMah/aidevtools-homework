235f1cbe4850d9319162c6685db267b4f8a98338
kaiquanmah <kaiquanmah@git.com>
2025-12-05 13:51:05 +0800

    fix python run crash issue in deploy container's website by sending same-origin, require-corp headers

--- 02-coding-interview/frontend/src/workers/pyodide.worker.js
+++ 02-coding-interview/frontend/src/workers/pyodide.worker.js
@@ -6,7 +6,7 @@ let pyodide = null;
 async function initPyodide() {
     if (!pyodide) {
         pyodide = await loadPyodide({
-            indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.29.0/full/',
+            indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.26.1/full/',
         });
         // Redirect stdout
         await pyodide.runPythonAsync(`
